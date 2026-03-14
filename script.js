374b69438a7298257f1b663511320805e90670d2
68319010027-hue <68319010027@loeitech.ac.th>
2026-03-14 22:43:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -319,15 +319,3 @@ function listenLogs() {
 
 listenAttendance();
 listenLogs();
-
-// ================= NAVBAR TOGGLE (MOBILE) =================
-document.addEventListener("DOMContentLoaded", () => {
-    const navToggle = document.getElementById("navToggle");
-    const navMenu = document.getElementById("navMenu");
-
-    if (navToggle && navMenu) {
-        navToggle.addEventListener("click", () => {
-            navMenu.classList.toggle("open");
-        });
-    }
-});
